c54ec343c4ba6cec59d28fd38d8362a5d8cf752d
T3thr <t.theerapat33@gmail.com>
2024-12-08 16:32:37 +0700

    m

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,15 +3,15 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import ChangeTheme from './ChangeTheme';  // Import the ChangeTheme component
-import { useTheme } from '@/context/Theme';  // Import the ThemeProvider hook
+import ChangeTheme from './ChangeTheme'; // Import the ChangeTheme component
+import { useTheme } from '@/context/Theme'; // Import the ThemeProvider hook
 import { useLanguage } from '@/context/Language';
 
 export default function NavBar() {
     const { language } = useLanguage();
     const pathname = usePathname();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const { isDarkMode, setIsDarkMode } = useTheme();  // Get the theme state from the provider
+    const { isDarkMode, setIsDarkMode } = useTheme(); // Get the theme state from the provider
 
     const styles = {
         active: 'text-blue-500 font-bold border-b-4 border-blue-500 transition-all duration-300',
@@ -38,24 +38,23 @@ export default function NavBar() {
 
                 {/* Theme Toggle Button */}
                 <div className="lg:flex lg:justify-between lg:mr-24 content-center">
-                <ChangeTheme isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+                    <ChangeTheme isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
                 </div>
+
                 {/* Navigation Links */}
                 <div
                     className={`${
                         isMenuOpen ? 'block' : 'hidden'
-                    } lg:flex lg:space-x-8 absolute lg:static top-16 left-0 w-full lg:w-auto  from-gray-800 via-gray-900 to-black text-white shadow-md lg:shadow-none lg:bg-transparent py-4 lg:py-0 z-50`}
+                    } lg:flex lg:space-x-8 absolute lg:static top-16 left-0 w-full lg:w-auto bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white shadow-md lg:shadow-none lg:bg-transparent py-4 lg:py-0 z-[9999]`}
                 >
-                    {[ 
+                    {[
                         { href: '/', label: language === 'en' ? 'Work' : 'ผลงาน' },
-                        { href: '/about', label: language === 'en' ? 'About' : 'เกี่ยวกับฉัน'  },
-
-
+                        { href: '/about', label: language === 'en' ? 'About' : 'เกี่ยวกับฉัน' },
                     ].map((navItem) => (
                         <Link
                             key={navItem.href}
                             href={navItem.href}
-                            className={`block px-6 py-2 lg:p-0 z-50 ${
+                            className={`block px-6 py-2 lg:p-0 ${
                                 pathname === navItem.href ? styles.active : styles.nonSelected
                             } relative group`}
                             onClick={() => setIsMenuOpen(false)}
@@ -63,7 +62,7 @@ export default function NavBar() {
                             {navItem.label}
                             {/* Span Animation for Non-Active Tabs */}
                             {pathname !== navItem.href && (
-                                <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full z-50"></span>
+                                <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
                             )}
                         </Link>
                     ))}
