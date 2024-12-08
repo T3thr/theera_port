f4bd0a7adcf1a674881802d105b604b88f2a04ff
T3thr <t.theerapat33@gmail.com>
2024-12-08 16:26:30 +0700

    m

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -63,7 +63,7 @@ export default function NavBar() {
                             {navItem.label}
                             {/* Span Animation for Non-Active Tabs */}
                             {pathname !== navItem.href && (
-                                <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+                                <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full z-50"></span>
                             )}
                         </Link>
                     ))}
